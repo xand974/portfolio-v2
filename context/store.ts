868a8471dc20d1df4069724831850e51fe05316f
xand974 <xand974@gmail.com>
2022-07-06 23:37:26 +0200

    refactor store

--- context/store.ts
+++ context/store.ts
@@ -5,6 +5,10 @@ export const store = configureStore({
   reducer: {
     modal: ModalSlice,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
--- modal/modal.service.ts
+++ modal/modal.service.ts
@@ -3,7 +3,6 @@ import { AppDispatch } from "../context/store";
 export class ModalService {
   static Open(element: JSX.Element, dispatch: AppDispatch) {
     dispatch(OPEN_MODAL(element));
-    console.log(element);
   }
 
   static Close(res: any, dispatch: AppDispatch) {
