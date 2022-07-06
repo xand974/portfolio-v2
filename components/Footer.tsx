1eb12868efd277aed5f448f3412eba71df9d11a2
xand974 <xand974@gmail.com>
2022-07-06 19:02:32 +0200

    add burger

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,23 +1,34 @@
+import { GitHub, LinkedIn } from "@mui/icons-material";
 import styles from "../styles/footer.module.css";
+import Image from "next/image";
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
       <div className={styles.footer__wrapper}>
         <div className={styles.footer__left}>
-          <h4 className={styles.footer__left__title}>
-            By <span>Alexandre Malet</span>{" "}
-          </h4>
+          <div className={styles.footer__left__title}>
+            <span className={styles.footer__left__title__italic}>By</span>
+            <span>Alexandre Malet</span>
+          </div>
         </div>
         <div className={styles.footer__right}>
           <a
             href="https://www.notion.so/malet/Doc_Web-1a167140aafd4540909b8b46575e31ee"
             target="_blank"
           >
-            My personal resources
+            <div className={styles.footer__right__svg}>
+              <Image
+                src="https://cdn.worldvectorlogo.com/logos/notion-logo-1.svg"
+                layout="fill"
+              />
+            </div>
           </a>
           <a href="https://github.com/xand974" target="_blank">
-            Source code
+            <GitHub className={styles.footer__right__icon} />
+          </a>
+          <a href="https://www.linkedin.com/in/alexandre-malet-5a3432208/">
+            <LinkedIn target="_blank" className={styles.footer__right__icon} />
           </a>
         </div>
       </div>
