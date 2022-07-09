6010b80f2a179b4c2da904e5966625c57e4e0704
xand974 <xand974@gmail.com>
2022-07-09 10:21:00 +0200

    fix path

--- next.config.js
+++ next.config.js
@@ -6,8 +6,8 @@ const nextConfig = {
     loader: "akamai",
     path: "",
   },
-  basePath: "/",
-  assetPrefix: "/",
+  basePath: "/portfolio-v2",
+  assetPrefix: "/portfolio-v2",
 };
 
 module.exports = nextConfig;
