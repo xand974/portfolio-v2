aa1ea66bf9b5946ad8030b58b81625e9f3e943b9
xand974 <75538669+xand974@users.noreply.github.com>
2022-07-09 10:11:16 +0000

    Deploying to gh-pages from @ xand974/portfolio-v2@63b416730b6e0cbf9239540d8321fd58a04678ae 🚀

---
+++ _next/static/qY6cqriklsUEWqwgLbPyJ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/531-f26be4337b606794.js","static/css/d5e16746550da79d.css","static/chunks/pages/index-f362c929f56d7e03.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
