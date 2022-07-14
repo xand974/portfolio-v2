7312d87e568001f3c79abdc99593dc867b7c9e8c
Alexandre Malet <alexandre.malet@betterfront.io>
2022-07-14 09:30:24 +0200

    init projects'

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -1,5 +1,22 @@
+import ProjectCard from "@/components/Projects/ProjectCard";
 import styles from "@/styles/projects.module.scss";
 
 export default function Projects() {
-  return <div className={styles.container}>PROJECTS</div>;
+  return <div className={styles.container}>
+    <div className={styles.container__grid}>
+      <ProjectCard />
+      <ProjectCard />
+      <ProjectCard />
+      <ProjectCard />
+      <ProjectCard />
+      <ProjectCard />
+      <ProjectCard />
+      <ProjectCard />
+      <ProjectCard />
+      <ProjectCard />
+      <ProjectCard />
+      <ProjectCard />
+      <ProjectCard />
+    </div>
+  </div>;
 }
